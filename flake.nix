c6db32b69b53a7c9a54912bc763a3d2695492520
Robbie Buxton <robbiesbuxton@gmail.com>
2024-01-16 16:37:54 +0000

    started work on compiling latex locally

--- flake.nix
+++ flake.nix
@@ -1,37 +1,95 @@
 {
-  description = "A flake for building Hello World";
+  description = "Build LaTeX document with minted";
 
+  inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-  outputs = { self, nixpkgs }: {
-
-    defaultPackage.x86_64-linux =
-      let
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      in
-      pkgs.stdenv.mkDerivation {
-        name = "hello-2.12.1";
-        src = self;
-        buildInputs = [ pkgs.gcc pkgs.gcc13 pkgs.gcc10 ];
-        configurePhase = "echo 'int main() { printf(\"Hello World!\"); }' > hello.c";
-        buildPhase = "gcc -o hello ./hello.c";
-        installPhase = "mkdir -p $out/bin; install -t $out/bin hello";
+  
+  outputs = { self, nixpkgs, flake-utils }:
+    {
+      templates.document = {
+        path = ./.;
+        description = "LaTeX document with minted support";
       };
 
+      lib.latexmk = import ./build-document.nix;
 
-    devShell.x86_64-linux =
+      defaultTemplate = self.templates.document;
+    } // flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      in
-      pkgs.mkShell {
-        buildInputs = [
+        pkgs = import nixpkgs { inherit system; };
+
+        latex-packages = with pkgs; [
+          (texlive.combine {
+            inherit (texlive)
+              scheme-full
+              framed
+              titlesec
+              cleveref
+              multirow
+              wrapfig
+              tabu
+              threeparttable
+              threeparttablex
+              makecell
+              environ
+              biblatex
+              biber
+              fvextra
+              upquote
+              catchfile
+              xstring
+              csquotes
+              minted
+              dejavu
+              comment
+              footmisc
+              xltabular
+              ltablex
+              ;
+          })
+          which
+          python39Packages.pygments
           (pkgs.inkscape-with-extensions.override {
             inkscapeExtensions = [ pkgs.inkscape-extensions.textext ];
           })
         ];
-      };
 
-  };
+        dev-packages = with pkgs; [
+          texlab
+          zathura
+          wmctrl
+        ];
+      in
+      rec {
+        devShell = pkgs.mkShell {
+          buildInputs = [ latex-packages dev-packages ];
+        };
+
+        packages = flake-utils.lib.flattenTree {
+          document = import ./build-document.nix {
+            inherit pkgs;
+            texlive = latex-packages;
+            shellEscape = true;
+            minted = true;
+            SOURCE_DATE_EPOCH = toString self.lastModified;
+          };
+        };
+
+        defaultPackage = packages.document;
+      }
+    );
 }
-#Not valid inside a nix flake but this is how I generated the dependency graph
-#nix-store --query --graph $(nix path-info .) | dot -Tpng -o out.png
+#Not avlid inside a nix flake but this is how I generated the dependency graph
+#nix-store --query --graph $(nix path-info .) | dot -Tsvg -o out.svg
+
+# pkgs.stdenv.mkDerivation {
+#   name = "hello-2.12.1";
+#   src = self;
+#   buildInputs = [ pkgs.gcc pkgs.gcc13 pkgs.gcc10 ];
+#   configurePhase = "echo 'int main() { printf(\"Hello World!\"); }' > hello.c";
+#   buildPhase = "gcc -o hello ./hello.c";
+#   installPhase = "mkdir -p $out/bin; install -t $out/bin hello";
+# };
+# inkscape = (pkgs.inkscape-with-extensions.override {
+#   inkscapeExtensions = [ pkgs.inkscape-extensions.textext ];
+# });
