2820fe2f3a062568e91ab30dbdaf84da7f96237d
Robbie Buxton <robbiesbuxton@gmail.com>
2024-01-22 19:37:13 +0000

    wip

--- latex/background/code/hello-nix.sh
+++ latex/background/code/hello-nix.sh
@@ -3,17 +3,20 @@ flake.lock  flake.nix
 
 [shell:~]$ nix flake show
 └──defaultPackage
-   └──x86\_64-linux: package 'hello-2.12.1'
+   └──x86_64-linux: package 'hello-2.12.1'
 
 [shell:~]$ nix run .
 Hello, world!
+
+[shell:~]$ nix path-info .
+\nix\store\sbldylj3clbkc0aqvjjzfa6slp4zdvlj-hello-2.12.1
 
 [shell:~]$ tree $(nix path-info .)
 "\nix\store\sbldylj3clbkc0aqvjjzfa6slp4zdvlj-hello-2.12.1"
 └──bin
     └──hello
 
-[shell:~]$ TODO get nix depencies
+[shell:~]$ nix-store --query $(nix path-info .) --requisites
 /nix/store/s2f1sqfsdi4pmh23nfnrh42v17zsvi5y-libunistring-1.1
 /nix/store/08n25j4vxyjidjf93fyc15icxwrxm2p8-libidn2-2.3.4
 /nix/store/lmidwx4id2q87f4z9aj79xwb03gsmq5j-xgcc-12.3.0-libgcc
